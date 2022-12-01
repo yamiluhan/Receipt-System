9c9a2d8bbe32f85819f10600933724e653c43343
jp <jp05harder@gmail.com>
2022-12-01 09:32:07 +0800

    sample

---
+++ src/array/Array.java
@@ -0,0 +1,18 @@
+
+package array;
+
+public class Array {
+
+    public static void main(String[] args) {
+      String names[] = {"jp harder","jason","jomar","jasmin","gk","lems","sir guy"};
+      System.out.println(names[0]); 
+         
+      
+      int number[] = {1,2,3,4,5,6,7,8,6,8,7};
+      System.out.println(number[6]+ number[8]); 
+      System.out.println(number[6]- number[8]); 
+      System.out.println(number[6]* number[8]); 
+      System.out.println(number[6]/ number[8]); 
+    }
+    
+}
